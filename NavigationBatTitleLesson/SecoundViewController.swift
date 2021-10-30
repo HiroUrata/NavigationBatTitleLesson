bbbcd8c8e5eadb2e62c852d15b92e0c2977600f2
HiroUrata <1712026@i.softbank.jp>
2021-10-30 18:42:14 +0900

    ViewController tableView cell 内容　追加

---
+++ NavigationBatTitleLesson/SecoundViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecoundViewController.swift
+//  NavigationBatTitleLesson
+//
+//  Created by UrataHiroki on 2021/10/30.
+//
+
+import UIKit
+
+class SecoundViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+  
+    
+}
--- NavigationBatTitleLesson/ViewController.swift
+++ NavigationBatTitleLesson/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let titleDatas = ["Swift","Kotlin","Dart","Python","Ruby","Java","PHP","Go","JavaScript","TypeScript","C#","C++",]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
+
+}
+
+extension ViewController:UITableViewDelegate{
+    
+ 
+}
+
+extension ViewController:UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return titleDatas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = titleDatas[indexPath.row]
+        
+        return cell
+    }
 
 }
 
